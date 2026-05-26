77cf393ea882fe0e53f5ab389060cfe889461d18
rajputroshansingh632-source <rajputroshansingh632@gmail.com>
2026-05-26 10:38:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 function createBot() {
 
   const bot = mineflayer.createBot({
-    host: 'gold.magmanode.com',
-    port: 25786,
+    host: 'infiXtream.aternos.me',
+    port: 22393,
     username: 'zeroxcheat',
     version: '1.21.4'
   });
